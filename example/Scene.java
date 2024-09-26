d5d6d9ae4cc8f39074b51927f3738a67b3db797e
Chun <caochun@gmail.com>
2024-09-26 09:56:46 +0800

    init

---
+++ example/Scene.java
@@ -0,0 +1,43 @@
+package example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.net.URL;
+
+import example.classloader.SteganographyClassLoader;
+
+public class Scene {
+
+    public static void main(String[] args) throws Exception {
+
+        Line line = new Line(7);
+        line.put(Gourd.ONE, 6);
+        line.put(Gourd.TWO, 3);
+        line.put(Gourd.THREE, 1);
+        line.put(Gourd.FOUR, 5);
+        line.put(Gourd.FIVE, 2);
+        line.put(Gourd.SIX, 4);
+        line.put(Gourd.SEVEN, 0);
+
+        Geezer theGeezer = Geezer.getTheGeezer();
+
+        SteganographyClassLoader loader = new SteganographyClassLoader(
+                new URL("http://java.nemoworks.info/images/example.BubbleSorter.png"));
+
+        Class c = loader.loadClass("example.BubbleSorter");
+
+        Sorter sorter = (Sorter) c.newInstance();
+
+        theGeezer.setSorter(sorter);
+
+        String log = theGeezer.lineUp(line);
+
+        BufferedWriter writer;
+        writer = new BufferedWriter(new FileWriter("result.txt"));
+        writer.write(log);
+        writer.flush();
+        writer.close();
+
+    }
+
+}
---
+++ example/Sorter.java
@@ -0,0 +1,11 @@
+package example;
+
+public interface Sorter {
+
+    public void load(int[] elements);
+
+    public void sort();
+
+    public String getPlan();
+
+}
